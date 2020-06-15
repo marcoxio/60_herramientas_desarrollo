29fa423d53f9b983952eafd156bb8b3eb692e1e1
Marco Jimenez <manjiso2@hotmail.com>
2020-06-15 08:39:12 -0500

    Ejemplo de arcText

--- master/lib/curso.js
+++ master/lib/curso.js
@@ -1,1 +1,23 @@
+var $texto = $("#txtArc");
 
+$texto.arctext({
+  radius: 300,
+});
+
+// $texto.arctext({
+//   radius: 300,
+//   dir: -1,
+// });
+
+// $texto.arctext({ radius: 500, rotate: false });
+
+setTimeout(function () {
+  $texto.arctext('set', {
+    radius: 300,
+    dir: -1,
+    animation: {
+      speed: 300,
+      easing: 'ease-out'
+    }
+  });
+}, 1000);
